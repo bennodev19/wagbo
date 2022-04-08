616b2e3d322d9d0a869099efc82cef93302225dd
benno <bennokohrs@gmail.com>
2022-04-08 07:00:37 +0200

    wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import {TwitterApi} from 'twitter-api-v2';
 import config from './config';
 import * as fs from "fs";
 import joinImages from "join-images";
+import {Canvas, Image, loadImage} from "canvas";
+import CanvasGrid from "merge-images-grid";
 
 const hashtag = '#WeAreOkay';
 
@@ -13,12 +15,25 @@ async function main() {
     // https://stackoverflow.com/questions/17369842/tile-four-images-together-using-node-js-and-graphicsmagick
     // https://www.youtube.com/watch?v=WtuJLcBvxI0
     const imageNames = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'];
-    console.log(imageNames.map(imageName => `${config.app.outImagesPath}/${imageName}`))
+    const imagePaths = imageNames.map(imageName => `${config.app.outImagesPath}/${imageName}`);
 
-    joinImages(imageNames.map(imageName => `${config.app.outImagesPath}/${imageName}`)).then((img) => {
+    joinImages(imagePaths).then((img) => {
         // Save image as file
         img.toFile(`${config.app.outPath}/out.png`);
     });
+
+    const images: {image: Image}[] = [];
+    for(const path of imagePaths){
+        const buffer = await loadImage(path);
+        images.push({image: buffer});
+    }
+    const merge = new CanvasGrid({
+        canvas: new Canvas(2, 2),
+        bgColor: '#fff',
+        list: images,
+    })
+    const buffer = merge.canvas.toBuffer();
+    fs.writeFileSync(`${config.app.outPath}/demo.png`, buffer);
 
     // // Query: https://developer.twitter.com/en/docs/twitter-api/tweets/counts/integrate/build-a-query
     // // Builder: https://developer.twitter.com/apitools/api?endpoint=%2F2%2Ftweets%2Fsearch%2Fall&method=get
